b0cdaea262996e72ed1cb58b4dba78f87f9437b0
joeyshi07 <joeys19@nycstudents.net>
2025-02-04 14:00:22 -0500

    changing struc

--- app/src/components/AlbumCard.vue
+++ app/src/components/AlbumCard.vue
@@ -1,13 +1,5 @@
 <template>
-  <div>
-    <h2>{{ albums.title }}</h2>
-    <h3>{{ albums.artist }}</h3>
-    <img :src="albums.image" :alt="albums.altText" />
-    <h3>{{ albums.price }}</h3>
-  </div>
+  <div v-for=""></div>
 </template>
 
-<script setup>
-import { albums } from './assets/albums.js'
-export default {}
-</script>
+<script setup></script>

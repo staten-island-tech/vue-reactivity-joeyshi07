714addeda1a60c262d95fef44f96491a526d9602
joeyshi07 <joeys19@nycstudents.net>
2025-02-07 13:38:06 -0500

    insert cards onto screen

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import AlbumList from '@/views/AlbumList.vue'
 </script>
 
 <template>
+  <AlbumList />
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 

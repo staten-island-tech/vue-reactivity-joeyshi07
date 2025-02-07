c54307815e84debb65fad4306d151a94c97eb76e
joeyshi07 <joeys19@nycstudents.net>
2025-02-07 14:01:12 -0500

    adding filtering

---
+++ app/src/components/AlbumFilter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    <button @click=""
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

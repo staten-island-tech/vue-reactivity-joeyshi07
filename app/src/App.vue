ea29dd3c5bc3ad67af5d3e7a72da0310b76cd236
joeyshi07 <joeys19@nycstudents.net>
2025-02-11 13:04:43 -0500

    add shopping cart to eh main page

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import AlbumList from '@/components/AlbumList.vue'
+import ShoppingCart from '@/components/ShoppingCart.vue'
 </script>
 
 <template>
